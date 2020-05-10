5c11b57ad16815074eb87ada3a4b3ad2f6d6e9ae
Matthew Karle <mkarle2178@gmail.com>
2020-05-10 15:57:17 -0400

    implemented simple clock

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { Animated, StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import ErrorBoundary from 'react-native-error-boundary';
 import styles from "./styles.js";
@@ -7,41 +7,72 @@ function Separator() {
     return <View style={styles.separator} />;
 }
 
-function Button(props) {
-    const rotate = new Animated.Value(0);
-    const onPress = () => {
-        rotate.setValue(0);
-        Animated.timing(rotate, {
-            toValue: 1,
-            duration: 1000,
-            useNativeDriver: true,
-        }).start();
+
+const formatNumber = number => `0${number}`.slice(-2);
+
+function convertSeconds(seconds) {
+    const hrs = Math.floor(seconds / 3600);
+    const remainder = seconds % 3600;
+
+    const mins = Math.floor(remainder / 60);
+    const secs = remainder % 60;
+
+    return { hrs: formatNumber(hrs), mins: formatNumber(mins), secs: formatNumber(secs) };
+}
+
+function Clock(props) {
+   
+    const [time, setTime] = useState(0); //state variable to track time in seconds
+    const [isActive, setActive] = useState(false); //state variable to track if clock is unpaused
+    const { hrs, mins, secs } = convertSeconds(time); //formatted time variables
+
+    const shadowRadius = useRef(new Animated.Value(0)).current;
+
+    const toggle = () => {
+        setActive(!isActive); //toggle paused/unpaused
+        
+        if(!isActive) {
+            Animated.timing(shadowRadius, {
+                toValue: 10,
+                duration: 1000,
+                useNativeDriver: true
+            }).start()
+        }
+        else{
+            shadowRadius.setValue(0)
+        }
     }
 
-    const rotation = rotate.interpolate({
-        inputRange: [0, 1],
-        outputRange: ['0deg', '360deg']
-    });
+    useEffect(() => {
+        let interval = null;
+        if(isActive) {
+            interval = setInterval( () => {
+                setTime(time + 1);
+            }, 1000);
+        }
+
+        return () => clearInterval(interval);
+    }, [isActive, time]);
 
     return (
         <TouchableOpacity
-            style={[styles.button, styles.buttonDim, {marginTop: 50, transform:[{rotate: rotation}]}]}
-            onPress={onPress}
+            style={[styles.button, styles.buttonDim, {marginTop: 50, shadowRadius: shadowRadius}]}
+            onPress={toggle}
         >
-            <Text style={styles.textButton}>{props.title}</Text>
+            <Text style={styles.textButton}>{hrs}:{mins}:{secs}</Text>
         </TouchableOpacity>
     );
 }
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Big Balled Boys</Text>
-      <Separator />
-      <ErrorBoundary>
-          <Button title="Press me"/>
+    <ErrorBoundary>
+        <View style={styles.container}>
+          <Text>Big Balled Boys</Text>
+          <Separator />
+          <Clock />
+        </View>
       </ErrorBoundary>
-    </View>
+
   );
 }
-
--- styles.js
+++ styles.js
@@ -3,38 +3,41 @@ const width = Dimensions.get('window').width;
 const height = Dimensions.get('window').height;
 
 export default styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-   buttonDim: {
-       padding: 5,
-       width: 100,
-       height: 100,
-       borderRadius: 200,
-   },
-   button: {
-       alignItems: 'center',
-       backgroundColor: "#2d2d2d",
-      justifyContent: "center",
-        margin: 3
-   },
-   buttonBorders: {
-       borderColor: "red",
-       borderWidth: 0,
-   },
-   textButton: {
-       color: "#f0fff0",
-   },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    buttonDim: {
+        padding: 5,
+        width: 100,
+        height: 100,
+        borderRadius: 200,
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: "#2d2d2d",
+        justifyContent: "center",
+        margin: 3,
+        shadowColor: '#9400D3',
+        shadowOffset: {width: 0, height: 0},
+        shadowOpacity: .8,
+        borderColor: "black",
+        borderWidth: 2,
+    },
+    textButton: {
+        color: "#f0fff0",
+    },
     separator: {
         marginVertical: 8,
         borderBottomColor: '#737373',
         backgroundColor: "#000000",
         borderBottomWidth: StyleSheet.hairlineWidth,
         width: (width - 100),
+    },
+    tickMark: {
+        borderLeftWidth: 2,
+        height: 10,
     }
-    
-
 });
